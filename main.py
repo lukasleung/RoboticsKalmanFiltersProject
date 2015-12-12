c1fd7dfcca6b442ba3c5f30964e1c82bde024a96
hoangvietnguyen <vinguyen@clarku.edu>
2015-12-12 16:32:40 -0500

    Complete Kalman function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from numpy.linalg import inv
 from numpy.random import normal
 
 
-def A(t):
+def state_transition_matrix(t):
     """ Compute the state transition matrix.
 
     Keyword arguments:
@@ -17,7 +17,7 @@ def A(t):
     ])
 
 
-def B(t):
+def control_matrix(t):
     """ Compute the control matrix.
     Express effects of acceleration on position and velocity.
 
@@ -33,7 +33,7 @@ def B(t):
 
 
 # system noise covariance matrix
-def E_X(t, sig_acceleration):
+def state_noise_covariance_matrix(t, sig_acceleration):
     sigma1 = 0.5 * t * t * sig_acceleration[0]
     sigma2 = 0.5 * t * t * sig_acceleration[1]
     sigma3 = t * sig_acceleration[0]
@@ -53,13 +53,14 @@ def generate_trajectory(initial_state, acceleration, number_of_iters, delta_t):
     Keyword arguments:
     initial_state -- [px_0, py_0, vx_0, vy_0]'
     acceleration -- [a_x, a_y]
-
     """
     states = []
     states.append(initial_state)
+    A = state_transition_matrix(delta_t)
+    B = control_matrix(delta_t)
     for i in range(1, number_of_iters):
         # state update
-        new_state = A(delta_t).dot(states[i - 1]) + B(delta_t).dot(acceleration)
+        new_state = A.dot(states[i - 1]) + B.dot(acceleration)
         states.append(new_state)
     return states
 
@@ -73,7 +74,6 @@ def generate_observations(true_trajectory, sig_obs):
     true_trajectory -- a list of true states of the object in motion,
         each a vector in R^4 = [p_x, p_y, v_x, v_y]
     sig_obs -- the covariance matrix of observation noise = [[sigma_x^2, 0], [0, sigma_y^2]]
-
     """
     obs = []
     n = len(true_trajectory)
@@ -86,37 +86,47 @@ def generate_observations(true_trajectory, sig_obs):
     return obs
 
 
-def kalman(initial_estimation, initial_obs, acceleration, number_of_iters, delta_t, sig_acceleration, sig_obs):
+def kalman(initial_estimation, obs, acceleration, number_of_iters, delta_t, sig_acceleration, sig_obs):
+    """
+    """
+    estimations = []
+    estimations.append(initial_estimation)
+
+    A = state_transition_matrix(delta_t)
+    A_transpose = A.transpose()
+    B = control_matrix(delta_t)
     C = np.matrix([[1, 0, 0, 0], [0, 1, 0, 0]])  # measurement matrix
     C_transpose = C.transpose()
+    E_X = state_noise_covariance_matrix(delta_t, sig_acceleration)
     P = np.zeros((4, 4), np.float_)  # a measure of the estimated accuracy of the state estimate
-    estimations = []
-    obs = []
-    estimations.append(initial_estimation)
-    obs.append(initial_obs)
+    I = np.identity(4)
+
     for i in range(1, number_of_iters):
-        t = delta_t * i
-        # estimation update
-        new_estimation = np.dot(A(t), estimations[i - 1]) + np.dot(B(t), acceleration)
-        estimations.append(new_estimation)
+        # estimation update by prediction
+        predicted_estimation = np.dot(A, estimations[i - 1]) + np.dot(B, acceleration)
         # update P
-        A_transpose = A(t).transpose()
-        P = (A(t).dot(P)).dot(A_transpose) + E_X(t, sig_acceleration)
-        # compute the Kalman gain
+        P = (A.dot(P)).dot(A_transpose) + E_X
+        # compute the Kalman gain (4 x 2 matrix)
         inverse = inv(C.dot(P).dot(C_transpose) + sig_obs)
         K = P.dot(C_transpose).dot(inverse)
+        # compute true estimation based on predicted value and new observation
+        true_estimation = predicted_estimation + K.dot(obs[i] - C.dot(predicted_estimation))
+        estimations.append(true_estimation)
+        # update P again
+        P = (I - K.dot(C)).dot(P)
 
+    return estimations
 
 if __name__ == "__main__":
-    # initial true state and estimation
     initial_estimation = np.array([0, 0, 0, 0]).reshape((4, 1))
     acceleration = np.array([1, 1]).reshape((2, 1))
     number_of_iters = 10
     delta_t = 1
     sig_acceleration = np.array([0.05, 0.05])  # std dev of noise of a_x, a_y, assume that they are independently distributed
     sig_obs = np.matrix([[1, 0], [0, 1]])  # std dev of noise of measurement (observation) of p_x and p_y
-    # kalman(initial_estimation, initial_obs, acceleration, number_of_iters, delta_t, sig_acceleration, sig_obs)
-    # true_trajectory = generate_trajectory(initial_estimation, acceleration, number_of_iters, delta_t)
-    # obs = generate_observations(true_trajectory, sig_obs)
-    # print true_trajectory
+    true_trajectory = generate_trajectory(initial_estimation, acceleration, number_of_iters, delta_t)
+    obs = generate_observations(true_trajectory, sig_obs)
+    estimations = kalman(initial_estimation, obs, acceleration, number_of_iters, delta_t, sig_acceleration, sig_obs)
+    print estimations
+    print true_trajectory
     # print obs
